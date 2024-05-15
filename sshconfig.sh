cd29f4a98b8a204e8c502835c1c0afe4e35a435b
lalfulsk <37483621+lalfulsk@users.noreply.github.com>
2024-05-15 16:19:36 +0530

    Update sshconfig.sh

--- sshconfig.sh
+++ sshconfig.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-# Define the ports and subsystem
-ports="Port 22
+# Define the SSH configuration
+ssh_config="
+Port 22
 Port 53
-Port 3303"
+Port 3303
+Protocol 2
+KeyRegenerationInterval 3600
+ServerKeyBits 1024
+SyslogFacility AUTH
+LogLevel INFO
+LoginGraceTime 120
+PermitRootLogin yes
+StrictModes yes
+RSAAuthentication yes
+PubkeyAuthentication yes
+IgnoreRhosts yes
+RhostsRSAAuthentication no
+HostbasedAuthentication no
+PermitEmptyPasswords no
+PermitTunnel yes
+ChallengeResponseAuthentication no
+PasswordAuthentication yes
+X11Forwarding yes
+X11DisplayOffset 10
+PrintMotd no
+PrintLastLog yes
+TCPKeepAlive yes
+AcceptEnv LANG LC_*
+Subsystem sftp /usr/lib/openssh/sftp-server
+UsePAM yes
+"
 
-subsystem="Subsystem sftp /usr/lib/openssh/sftp-server"
-
-# Append the ports and update the subsystem in the SSH configuration file
-echo -e "$ports\n$subsystem" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+# Update the SSH configuration file
+echo "$ssh_config" | sudo tee /etc/ssh/sshd_config > /dev/null
 
 # Restart the SSH service
 sudo systemctl restart ssh
